0640302c9c2c723602890a947201ba9cc539b553
MounirBlk <mounir.ballouk@gmail.com>
2020-02-09 14:21:45 +0100

    appareil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Lucien';
   isAuth = false;
+
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+
   appareils = [
     {
       name: 'Machine à laver',
